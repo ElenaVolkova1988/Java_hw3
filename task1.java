afa70f882f9211ffa44ce1947df7ca3be8fbe3d4
Elena <xoroshj_chelovek@mail.ru>
2023-02-21 18:25:55 +0300

    first commit

---
+++ task1.java
@@ -0,0 +1,21 @@
+//1.Реализовать алгоритм сортировки слиянием
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.createRandomArray;
+
+public class task1 {
+    
+    public static void main(String[] args){
+    
+        List<List<Integer>> matrix = new ArrayList<>();
+        matrix.add(Arrays.asList(1, 2, 3));
+        matrix.add(Arrays.asList(4, 5, 6));
+        matrix.add(Arrays.asList(7, 8, 9));
+      
+    }
+
+    
+}
